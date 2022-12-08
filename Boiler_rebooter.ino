a3bb72a101350d3a02a596fbf494125297e04cee
Yaroslav Emelyanov <emel.yar95@gmail.com>
2022-12-08 20:10:11 +0400

    Implements algorithm using classes

--- Boiler_rebooter.ino
+++ Boiler_rebooter.ino
@@ -1,5 +1,6 @@
-#include <Servo.h>
-#include <LiquidCrystal.h>
+#include "Detector.h"
+#include "StateMonitor.h"
+#include "ButtonPusher.h"
 
 constexpr uint8_t POT_PIN = A0;
 
@@ -22,80 +23,37 @@ constexpr uint8_t SERVO_MAX_ANGLE = 180;
 
 constexpr uint8_t BRIGHTNESS_THRESHOLD = 300;
 
-LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_EN, LCD_PIN_DB4, LCD_PIN_DB5, LCD_PIN_DB6, LCD_PIN_DB7);
-Servo servo;
-
-int attemptsCount = 0;
-
-struct ButtonPresser {
-  
-};
+Detector detector(POT_PIN, LED_PIN, BRIGHTNESS_THRESHOLD);
+StateMonitor stateMonitor(LCD_PIN_RS, LCD_PIN_EN, LCD_PIN_DB4, LCD_PIN_DB5, LCD_PIN_DB6, LCD_PIN_DB7);
+ButtonPusher buttonPusher(SERVO_PIN, SERVO_DEFAULT_ANGLE, SERVO_PRESS_BUTTON_ANGLE, SERVO_MAX_ANGLE);
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUZZER_PIN, OUTPUT);
-  servo.attach(SERVO_PIN);
-
-  lcd.begin(16, 2);
-
-  moveServo(SERVO_DEFAULT_ANGLE);
 }
 
 void loop() {
-  if (checkHeaterAndUpdateLed()) {
+  if (detector.updateAndGetState()) {
     pressHeaterResetButton();
   } else {
-    attemptsCount = 0;
+    
   }
-
-}
-
-/**
-  * Updates LED status. On — if if boiler needs to be restarted, off — overwise
-  *
-  * @returns true if boiler needs to be restarted 
-**/
-bool checkHeaterAndUpdateLed() {
-  bool heaterLedIsOn = analogRead(POT_PIN) > BRIGHTNESS_THRESHOLD;
-  digitalWrite(LED_PIN, heaterLedIsOn);
-  return heaterLedIsOn;
-}
-
-void setScreenState() {
-  
 }
 
 void pressHeaterResetButton() {
-  if (attemptsCount == 3) {
-    requestManualReset();
+  if (stateMonitor.getAttemptNumber() == 3) {
+    stateMonitor.displayManualResetRequest();
     return;
   }
 
-  moveServo(SERVO_PRESS_BUTTON_ANGLE);
-  moveServo(SERVO_DEFAULT_ANGLE);
-
-  attemptsCount++;
-  //playResetPressed();
+  buttonPusher.pushButton();
+  stateMonitor.increaseAttemptNumber();
 
   delay(10000);
 
-  /*if (checkHeaterLedAndUpdateLed()) {
+  if (detector.updateAndGetState()) {
     pressHeaterResetButton();
-  }*/
+  }
 }
 
-void requestManualReset() {
-
-  tone(BUZZER_PIN, 3000, 250);
-  tone(BUZZER_PIN, 3000, 250);
-  tone(BUZZER_PIN, 3000, 250);
-}
-
-void moveServo(int angle) {
-  angle = int(angle % SERVO_MAX_ANGLE);
-  servo.write(angle);
-  delay(250);
-}
 
 
 
